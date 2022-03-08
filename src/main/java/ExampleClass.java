0647d445c1443cfb0c83b97008e5932a5d5ec247
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-08 15:27:42 -0600

    pushing up project structure

---
+++ src/main/java/ExampleClass.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+
+public class ExampleClass {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.reverse("Jonathan"));
+
+    }
+}
