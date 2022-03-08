12c721ccb1b8b7fc8c8ec6fb1d1b29212f278ed4
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-08 15:39:26 -0600

    finished managing dependencies exercise

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -5,7 +5,11 @@ import org.apache.commons.lang3.SystemUtils;
 public class ExampleClass {
 
     public static void main(String[] args) {
-        System.out.println(StringUtils.reverse("Jonathan"));
+        String string = "Life, The Universe and Everything";
+        System.out.println("You entered: " + string);
+        System.out.println("StringUtils.isNumeric(string) = " + StringUtils.isNumeric(string));
+        System.out.println("StringUtils.swapCase(string) = " + StringUtils.swapCase(string));
+        System.out.println("StringUtils.reverse(string) = " + StringUtils.reverse(string));
 
     }
 }
